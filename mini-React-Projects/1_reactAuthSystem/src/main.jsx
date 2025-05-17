42f33a17e800dfdb9fe24e1fd2640fcbb3d3c6cf
Aniketkole27 <aniketkole07@gmail.com>
2025-05-17 09:07:39 +0530

    feat: implement Layout component and configure routing with react-router-dom

--- mini-React-Projects/1_reactAuthSystem/src/main.jsx
+++ mini-React-Projects/1_reactAuthSystem/src/main.jsx
@@ -3,8 +3,36 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Router,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+import Layout from "./Layout.jsx";
+import Home from "./components/Home/Home.jsx";
+import Login from "./components/Login/Login.jsx";
+import Resister from "./components/Resister/Resister.jsx";
+import { AuthContext2Provider } from "./context/AuthContext2.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Resister />} />
+      
+    </Route>
+  )
+);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <App />
+    <AuthContext2Provider>
+    <RouterProvider router={router} />
+    </AuthContext2Provider>
+    {/* <App /> */}
   </StrictMode>
 );
