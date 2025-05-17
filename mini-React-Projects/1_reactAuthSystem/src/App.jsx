e8a8f1f591a435291b2eb94652740f12808971a2
Aniketkole27 <aniketkole07@gmail.com>
2025-05-17 09:07:03 +0530

    refactor: update App component structure and import statements for authentication context

--- mini-React-Projects/1_reactAuthSystem/src/App.jsx
+++ mini-React-Projects/1_reactAuthSystem/src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-// import Resister from "./components/Resister/Resister";
-import { AuthProvider } from "./context/AuthContext.js";
-import AuthContext from "./context/AuthContext";
+import Resister from "./components/Resister/Resister";
+// import { AuthProvider } from "./context/AuthContext.js";
+// import AuthContext from "./context/AuthContext";
+
+import { AuthContext2Provider } from "./context/AuthContext2";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+
+
 
 function App() {
   return (
-    <AuthProvider value={{isAuthenticated}}>
-      <h1 className="text-3xl py-2 text-white">
+    <AuthContext2Provider>
+      {/* <h1 className="text-3xl py-2 text-white">
         React Auth System with Router Protection
-      </h1>
-    </AuthProvider>
+      </h1> */}
+      {/* <Resister /> */}
+      {/* <Login /> */}
+      {/* <Header /> */}
+      <Home />
+    </AuthContext2Provider>
   );
 }
 
