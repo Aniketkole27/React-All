9a71611ad533c89c8523280ac9814017b101e654
Aniketkole27 <aniketkole07@gmail.com>
2025-05-23 14:03:20 +0530

    feat: refactor todoSlice to manage todos with an initial state and implement add, remove, and update reducers

--- LearningReact/10_reduxToolKitTodo/src/features/Todo/todoSlice.js
+++ LearningReact/10_reduxToolKitTodo/src/features/Todo/todoSlice.js
@@ -1,8 +1,48 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, TaskAbortError } from "@reduxjs/toolkit";
+import { useState } from "react";
 
-const initialState = [
-    {
-        id:1,
-        text : 'hello'
-    }
-]
+const initialState = {
+  todos: [],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        // text: action.payload.text;
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+
+    removeTodo: (state, action) => {
+      // const id = action.payload.id
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+
+    updateTodo: (state, action) => {
+      // state.todos.map((todo)=>{
+      //     if(todo.id === action.payload.id){
+      //         todo.text = action.payload.text
+      //     }
+      // })
+      const todo = state.todos.find((todo) => todo.id === action.payload.id);
+      if (todo) {
+        todo.text = action.payload.text;
+      }
+    },
+  },
+});
+
+export const {
+  addTodo,
+  removeTodo,
+  updateTodo,
+  updateValue,
+  status,
+  setStatus,
+} = todoSlice.actions;
+export default todoSlice.reducer;
