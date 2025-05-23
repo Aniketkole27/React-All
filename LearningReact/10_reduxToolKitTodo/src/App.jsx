b4a4d728fb006a72449119af606d007bd3cb4a47
Aniketkole27 <aniketkole07@gmail.com>
2025-05-23 14:03:35 +0530

    feat: add EditorComponent and Todos components to App for enhanced functionality

--- LearningReact/10_reduxToolKitTodo/src/App.jsx
+++ LearningReact/10_reduxToolKitTodo/src/App.jsx
@@ -1,11 +1,17 @@
 
 import './App.css'
+import AddTodo from './componenets/AddTodo'
+import EditorComponent from './componenets/Editor'
+import Todos from './componenets/Todos'
 
 function App() {
 
   return (
     <>
       <h1 className='text-white text-4xl'>Redux Toolkit</h1>
+      {/* <EditorComponent /> */}
+      <AddTodo />
+      <Todos />
     </>
   )
 }
