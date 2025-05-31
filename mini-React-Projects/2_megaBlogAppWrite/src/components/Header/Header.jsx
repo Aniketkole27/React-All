aab16fd4db9a3736ed9afe70da501ce828311936
Aniketkole27 <aniketkole07@gmail.com>
2025-05-31 12:01:52 +0530

    feat: add env dependency and improve button styles in Header and LogoutBtn components

--- mini-React-Projects/2_megaBlogAppWrite/src/components/Header/Header.jsx
+++ mini-React-Projects/2_megaBlogAppWrite/src/components/Header/Header.jsx
@@ -48,10 +48,10 @@ function Header() {
           <ul className="flex ml-auto">
             {navItems.map((item) =>
               item.active ? (
-                <li key={item.nama}>
+                <li key={item.nama} className="mr-2">
                   {/* <Link to={item.slug}>{item.nama}</Link> */}
                   <button
-                    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full cursor-pointer"
                     onClick={() => navigate(item.slug)}
                   >
                     {item.nama}
